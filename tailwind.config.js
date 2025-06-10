a9dbe66ad54856778d978f75a8339d531582869e
Guvnor64 <kisieltony@gmail.com>
2025-06-10 12:35:53 +0200

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,20 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        heilnetzRed: '#D72638',
+        heilnetzGray: '#3C3C3C'
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
